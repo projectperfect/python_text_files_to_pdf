6315e3d6f0bc2178731e5a094fcc193819c83af4
projectperfect <jprfct@protonmail.com>
2023-04-08 23:11:33 -0700

    project completed

---
+++ main.py
@@ -0,0 +1,35 @@
+import glob
+from fpdf import FPDF
+from pathlib import Path
+
+# Creates a list of text filepaths
+filepaths = glob.glob("text_files/*.txt")
+
+# Creates one PFD file
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+# Go through each text file
+for filepath in filepaths:
+    # Add a page to the PDF document for each text file
+    pdf.add_page()
+
+    # Get the filepath without the extension
+    # and convert it to title case (e.g. Cat)
+    filename = Path(filepath).stem
+
+    # Add the name to the PDF
+    name = filename.title()
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=name, ln=1)
+
+    # Get the content of each file
+    with open(filepath, "r") as file:
+        content = file.read()
+    # Add the text file content to the PDF
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
+
+
+# Produce the PDF
+pdf.output("PDFs/output.pdf")
+
